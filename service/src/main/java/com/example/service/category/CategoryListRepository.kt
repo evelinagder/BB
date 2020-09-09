74a8f5dffce74eeb36c92cedf0a6b6f2aea08508
ederventska <uBr+WRoQ1dr&v>
2020-09-09 13:38:21 +0300

    permissions, setup call

--- service/src/main/java/com/example/service/category/CategoryListRepository.kt
+++ service/src/main/java/com/example/service/category/CategoryListRepository.kt
@@ -2,10 +2,14 @@ package com.example.service.category
 
 import com.example.service.ChuckNorrisApi
 import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class CategoryListRepository @Inject constructor(val api: ChuckNorrisApi) {
 
     fun getCategories(): Single<List<String>> = api.getCategories()
-
+        .subscribeOn(Schedulers.io())
+        .observeOn(
+        AndroidSchedulers.mainThread())
 }
