ef2ce66c373124ed8adc699f763405d3963e40cc
ederventska <uBr+WRoQ1dr&v>
2020-09-10 14:39:14 +0300

    toolbar text

--- app/src/main/java/com/example/chuck/categorydetail/RandomJokeFragment.kt
+++ app/src/main/java/com/example/chuck/categorydetail/RandomJokeFragment.kt
@@ -5,7 +5,6 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.Navigation
 import com.bumptech.glide.Glide
 import com.example.chuck.R
 import com.example.chuck.di.ViewModelFactory
